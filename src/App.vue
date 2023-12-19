1742f1962b28912884cbd6fd728d1524172e3fcb
arianspacerock <siropemptyspace@gmail.com>
2023-12-19 06:11:32 +0300

    init: add header & logo

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,38 @@
 </script>
 
 <template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+
+    <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="Logo" class="w-10">
+        <div>
+          <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+          <p class="text-slate-500">Best sneakers shop</p>
+        </div>
+      </div>
+
+      <ul class="flex items-center gap-10">
+        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+          <img src="/cart.svg" alt="Cart">
+          <b>0$</b>
+        </li>
+
+        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+          <img src="/heart.svg" alt="Cart">
+          <span>Закладки</span>
+        </li>
+
+        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+          <img src="/profile.svg" alt="Cart">
+          <span>Профиль</span>
+        </li>
+      </ul>
+
+    </header>
+
+  </div>
 
 </template>
 
