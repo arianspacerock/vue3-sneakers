6c763d0fadc001d3e768a1b57d095d864faae432
arianspacerock <siropemptyspace@gmail.com>
2024-01-09 16:48:48 +0300

    init: add filters

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch, reactive} from "vue";
 import axios from "axios";
 
 import Header from './components/Header.vue'
@@ -8,9 +8,27 @@ import Drawer from "@/components/Drawer.vue";
 
 const items = ref([]) // { value: [] }
 
+const filters = reactive({
+  sortBy: '',
+  searchQuery: '',
+})
+
+const onChangeSelect = event => {
+  filters.sortBy = event.target.value
+}
+
 onMounted(async () => {
   try {
     const {data} = await axios.get('https://3740aa96c0805c9e.mokky.dev/items')
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+watch(filters,async () => {
+  try {
+    const {data} = await axios.get('https://3740aa96c0805c9e.mokky.dev/items?sortBy=' + filters.sortBy)
     items.value = data
   } catch (err) {
     console.log(err)
@@ -27,10 +45,10 @@ onMounted(async () => {
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
         <div class="flex gap-4">
-          <select class="py-2 px-3 outline-none cursor-pointer border rounded-md">
-            <option value="">По названию</option>
-            <option value="">По цене (дешевые)</option>
-            <option value="">По цене (дорогие)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 outline-none cursor-pointer border rounded-md">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
 
           <div class="relative">
