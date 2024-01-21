90734812d2fa4063ac310892ddded6dba843ccef
arianspacerock <siropemptyspace@gmail.com>
2024-01-21 17:24:21 +0300

    init: add logic for favorites

--- src/pages/Favorites.vue
+++ src/pages/Favorites.vue
@@ -1,5 +1,19 @@
 <script setup>
+ import { ref, onMounted} from "vue";
+ import axios from "axios";
 
+ const favorites = ref([])
+
+
+ onMounted(async () => {
+   try {
+     const {data} = await axios.get('https://3740aa96c0805c9e.mokky.dev/favorites?_relations=items')
+
+     favorites.value = data
+   } catch (err) {
+     console.log(err)
+   }
+ })
 </script>
 
 <template>
